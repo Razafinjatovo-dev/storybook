754288a987a957cd2b77e7e141d2131c45b04f12
Joel-Razafinjatovo <raza.joel@gmail.com>
2021-02-24 14:39:13 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Top from "./Components/Top";
+import Select from "./Components/Select";
+import Button from "./Components/Button";
+import ShopDetails from "./Components/ShopDetails";
+import "./App.css";
 
 function App() {
+  const [selectedShop, setSelectedShop] = useState("");
+  const [displayAdress, setDisplayAdress] = useState(false);
+  const [shopToDisplay, setShopToDisplay] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='FormWrapper'>
+        <Top />
+        <Select
+          setSelectedShop={setSelectedShop}
+          setDisplayAdress={setDisplayAdress}
+          setShopToDisplay={setShopToDisplay}
+        />
+        <ShopDetails
+          selectedShop={selectedShop}
+          displayAdress={displayAdress}
+          shopToDisplay={shopToDisplay}
+        />
+
+        <Button
+          displayAdress={displayAdress}
+          setDisplayAdress={setDisplayAdress}
+          selectedShop={selectedShop}
+        />
+      </div>
     </div>
   );
 }
